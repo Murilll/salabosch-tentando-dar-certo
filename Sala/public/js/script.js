63ffc8a1868b762b2f8ab4d7ba72f3376f41d49d
Murilll <murilosertorio25@gmail.com>
2023-01-13 13:38:32 -0300

    ._.

---
+++ Sala/public/js/script.js
@@ -0,0 +1,5 @@
+function headerBosch(){
+    document.getElementById("headerBosch").style.height = "1vh";
+    document.getElementById("body").style.overflow = "scroll";
+    // document.getElementById("header").style.transition = "1s";
+}
